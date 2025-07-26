f82f4b78dfbc0b967b18833f4e6e2e903b630136
MithYadav <yadav.kumar0118@gmail.com>
2025-07-26 17:45:16 +0530

    updated amazon AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_instance" "springboot" {
-  ami                         = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  ami                         = "ami-0d0ad8bb301edb745" # Amazon Linux 2
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
